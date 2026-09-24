9ad88d0e60ba93373b70693d266251b2e397ea1d
Shivraj486 <shivamchhatani486@gmail.com>
2026-09-24 21:29:33 +0500

    Create task8.c

---
+++ task8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int num1,num2;
+  printf("Enter First NUmber: \n");
+  scanf("%d" , &num1);
+  printf("Enter Second Number :\n");
+  scanf(" %d" , &num2);
+  printf("SUM :%d\n" , num1 + num2);
+  printf("Difference :%d\n" , num1 - num2);  
+  printf("Product :%d\n" , num1 * num2);
+  if (num != 0) {
+    printf("Quotient: %.2f\n" , num1 / num2);
+  } else
+  {
+    printf("Quotient: Undefined\n");
+return 0;
+}
